0d91451517dc36675dd3f65eaf70770fa283b497
vladyslavkosenko <vaduha91@gmail.com>
2022-08-26 14:58:51 +0300

    created a project

---
+++ src/main/java/com/example/SpringDataApp/SpringDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataApp/SpringDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
